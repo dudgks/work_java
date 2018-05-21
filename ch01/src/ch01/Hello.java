921b32911208268f286bc0d1c822c748cfd40c77
KOITT <KOITT@KOITT-PC>
2018-05-21 21:24:36 +0900

    두 번째 코드 작성

--- ch01/src/ch01/Hello.java
+++ ch01/src/ch01/Hello.java
@@ -5,7 +5,7 @@ public class Hello {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
         System.out.println("Hello, world!");
-      
+        System.out.println("안녕, 세계.");
 	}
 
 }
