74a6d9af7296a9373cce689325cf2ec368b11895
KOITT <KOITT@KOITT-PC>
2018-06-08 21:44:48 +0900

    4장 진단문제

--- Ch04/src/ch04/Ch04Ex01_05.java
+++ Ch04/src/ch04/Ch04Ex01_05.java
@@ -4,20 +4,24 @@ import java.util.Scanner;
 
 public class Ch04Ex01_05 {
 	public static void main(String[] args) {
-		
-		double grade = 0.0;
-		
-	Scanner scanner = new Scanner(System.in);
-	String[] numbers = scanner.nextLine().split(" ");
-	double a = Double.parseDouble(numbers[0]);
-	double b = Double.parseDouble(numbers[1]);
-	String[] numbers1 = scanner.nextLine().split(" ");
-	double c = Double.parseDouble(numbers[2]);
-	double d = Double.parseDouble(numbers[3]);
-	
-	switch (grade) { 
-	
-	}
+
+		double x = 0.0;
+		Scanner scanner = new Scanner(System.in);
+		String[] numbers = scanner.nextLine().split(" ");
+		double a = Double.parseDouble(numbers[0]);
+		double b = Double.parseDouble(numbers[1]);
+		String[] numbers1 = scanner.nextLine().split(" ");
+		double c = Double.parseDouble(numbers[0]);
+		double d = Double.parseDouble(numbers[1]);
+
+		if(x >= 4.0) { 
+			System.out.println('A');
+		} if (x >= 3.0) {
+			System.out.println('B');		
+		}else {
+			System.out.println('C');
+		}
+
 	}
 
 }
