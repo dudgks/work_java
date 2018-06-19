70f732ead178ec202f548f355e28b61e52c1a3f9
KOITT <KOITT@KOITT-PC>
2018-06-19 21:46:06 +0900

    5장 진단문제

--- ch05/src/ch05/Ch05Ex01_02.java
+++ ch05/src/ch05/Ch05Ex01_02.java
@@ -2,19 +2,26 @@ package ch05;
 
 public class Ch05Ex01_02 {
 	public static void main(String[] args) {
-		int i, j = 1;
-		int[] arr = new int [10];
+		// 방법 1
+		int[] numbers = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+		for(int i = 0; i < numbers.length; i++) {
+			System.out.print(numbers[i] + " ");
+		}
+		System.out.println();
 
-		 for (i = 0; i < 10; i++) {
-		  arr[i] = j;
-		  j++;
-		 }
+		// 방법 2
+		int[] numbers2 = new int [10];		
+		for( int i = 0; i < numbers2.length; i++) { 
+			numbers2[i] = i + 1;
+		}
 
-		 for (i = 0; i < 10; i++) {
-		  System.out.printf("%d ", arr[i]);
-		 }
+		for ( int i = 0; i < numbers2.length; i++) { 
+			System.out.print(numbers2[i] + " ");
+		}
+
 	}
 }
+
 
 
 
--- ch05/src/ch05/Ch05Ex01_03.java
+++ ch05/src/ch05/Ch05Ex01_03.java
@@ -4,20 +4,18 @@ import java.util.Scanner;
 
 public class Ch05Ex01_03 {
 	public static void main(String[] args) {
-		int[] arr = new int[10];
-		int i;
-
-		Scanner scanner = new Scanner(System.in); 
-		String input = scanner.nextLine();
-
-		for (i = 0; i < arr.length; i++) {
-				System.out.printf(" %c", arr[i]);    
-				break;
-			}
-			System.out.printf(" %c %c %c", (char)arr[0], (char)arr[3], (char)arr[6]);
-			System.out.println(arr[i]);
-		}
-
+		// 1. 가로로 입력받기
+		Scanner scanner = new Scanner(System.in);
+		String[] alphabets = scanner.nextLine().split(" ");
+		
+		// 2. 첫 번째 문자 출력
+		System.out.print(alphabets[0] + " ");
+		
+		// 3. 네 번째 문자 출력
+		System.out.print(alphabets[3] + " ");
+		
+		// 4. 일곱 번째 문자 출력
+		System.out.print(alphabets[6] + " ");
 
 	}
-
+}
