1d287242e085250c430d38aee1fcbe36f9f019a3
KOITT <KOITT@KOITT-PC>
2018-06-22 21:22:51 +0900

    5장 실전문제

--- ch05/src/ch05/Ch05Ex02_01.java
+++ ch05/src/ch05/Ch05Ex02_01.java
@@ -5,24 +5,15 @@ import java.util.Scanner;
 public class Ch05Ex02_01 {
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
-		String[] numbers = scanner.nextLine().split(" ");
-		
-		for(int i = 0; i < numbers.length; i++) {
-			boolean changed = false; // 자리바꿈 체크
-			
-			for(int j = 0; j < numbers.length - 1 - i; j++) {
-				if(numbers[j] > numbers[j + 1]) {		// 옆의 값이 작으면 서로 바꾼다.
-					String temp = numbers[j];
-					numbers[j] = numbers[j + 1];
-					numbers[j + 1] = temp;
-					changed = true;
-		}
+		String[] chars = scanner.nextLine().split(" ");
 				
-		
-		
-		
+		// 입력받은 문자열들을 배열 뒤에서부터 출력
+		for(int i = chars.length - 1; i >= 0 ; i ++) {
+			System.out.print(chars[i] + " ");
+		}		
 	}
+}
 
-}
-	}
-}
+
+
+
